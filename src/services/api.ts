4bca0f101dfd200a1252df2197e3a9c5d47babc5
Sarah Ali <sarahali6233@gmail.com>
2025-01-19 21:28:13 +0100

    Update API_URL to production server

--- src/services/api.ts
+++ src/services/api.ts
@@ -21,7 +21,7 @@ export interface ShorthandCategory {
   rules: ShorthandRule[];
 }
 
-export const API_URL = "http://localhost:5001";
+export const API_URL = "https://shrthnder-server.vercel.app";
 
 export const login = async (
   email: string,
